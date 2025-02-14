6cd13a55657ca66e62028bd2141964cebc021006
osama <115135285+Osss1000@users.noreply.github.com>
2025-02-14 23:27:11 +0200

    Add files via upload

---
+++ task 22_1/SalesDatabase/Data/SalesContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P02_SalesDatabase.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P02_SalesDatabase.Data
+{
+    internal class SalesContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Store> Stores { get; set; }
+        public DbSet<Sale> Sales { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("server=localhost ; Initial Catalog=SalesDatabase ; Integrated Security = True ; TrustServerCertificate = True ");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Product>().Property(e=>e.Name).HasColumnType("varchar(50)").IsUnicode(true);
+            modelBuilder.Entity<Customer>().Property(e => e.Name).HasColumnType("varchar(100)").IsUnicode(true);
+            modelBuilder.Entity<Customer>().Property(e => e.Email).HasColumnType("varchar(80)").IsUnicode(false);
+            modelBuilder.Entity<Store>().Property(e => e.Name).HasColumnType("varchar(80)").IsUnicode(true);
+            modelBuilder.Entity<Product>().Property(e => e.Description).HasColumnType("varchar(250)").HasDefaultValue("No description");
+            modelBuilder.Entity<Sale>().Property(e => e.Date).HasDefaultValueSql("GETDATE()");
+        }
+    }
+}
---
+++ task 22_1/SalesDatabase/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P02_SalesDatabase.Models
+{
+    internal class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public int price { get; set; }
+        public List<Sale> sales { get; set; }
+        public string Description { get; set; }
+
+
+    }
+}
